c252fe1b1290fa4270c5dee7a9dd657675d8eb5e
gautamomento <gautam@momentohq.com>
2021-10-22 14:37:40 -0700

    prevent multiple versions for dev

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ import time
 
 version = os.getenv("PYPI_MOMENTO_WIRE_TYPE_VERSION")
 if [version == None]:
-    version = '0.0.experimental-' + str(time.time()*1000)
+    version = '0.0.dev'
 
 # version is the only dynamic configuration
 setuptools.setup(
