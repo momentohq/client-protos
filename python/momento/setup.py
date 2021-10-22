518f16162b6317a3256ea7cf47da0924b76826f9
gautamomento <gautam@momentohq.com>
2021-10-22 13:51:33 -0700

    generate experimental versions

--- python/momento/setup.py
+++ python/momento/setup.py
@@ -1,9 +1,10 @@
 import setuptools
 import os
+import time
 
 version = os.getenv("PYPI_MOMENTO_WIRE_TYPE_VERSION")
 if [version == None]:
-    version = '0.0.dev'
+    version = '0.0.experimental-' + str(time.time()*1000)
 
 # version is the only dynamic configuration
 setuptools.setup(
