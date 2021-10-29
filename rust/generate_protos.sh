6254421e0ffb2993b4eeef1431709ebb6528b3ee
Matt Straathof <matt.straat@gmail.com>
2021-10-29 14:44:56 -0700

    fix: dont commit proto code

--- rust/generate_protos.sh
+++ rust/generate_protos.sh
@@ -2,4 +2,17 @@
 set -e
 set -x
 
+CURRENT_DIR=${PWD##*/}
+if [ "$CURRENT_DIR" != "rust" ]; then
+        echo "run this script rust directory, current directory is ${CURRENT_DIR}"
+        exit 1
+fi
+
+mkdir src
+pushd src
+    echo "mod cacheclient;" >> lib.rs
+    echo "mod controlclient;" >> lib.rs
+    echo "" >> lib.rs
+popd
+
 protoc -I=../proto --rust_out=./src cacheclient.proto controlclient.proto
