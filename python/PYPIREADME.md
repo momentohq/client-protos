6bbeb09cebf7646c398f3b63edc794765d8385c6
Erika Tharp <eem@momentohq.com>
2022-02-09 11:36:26 -0800

    chore: Open source and publish to PyPI (#31)
    chore: Open source and publish to PyPI

---
+++ python/PYPIREADME.md
@@ -0,0 +1,3 @@
+# Momento Client Proto Generated Files for Python
+
+This package includes gRrpc clients and the response and request classes to interact with Momento Services.
--- python/setup.py
+++ python/setup.py
@@ -1,12 +1,19 @@
 import setuptools
 import os
-import time
 
 version = os.getenv("PYPI_MOMENTO_WIRE_TYPE_VERSION")
 
 assert version != None
 
-# version is the only dynamic configuration
+with open("PYPIREADME.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     version=version,
+    license_files = ('../LICENSE.txt',),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_urls={
+        'Source': 'https://github.com/momentohq/client_protos',
+    },
 )
