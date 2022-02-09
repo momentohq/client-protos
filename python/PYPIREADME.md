0339caacb66c252daf917c956f6f1f232fb4ed34
erikatharp <eem@momentohq.com>
2022-02-08 18:04:40 -0800

    chore: add simple readme for PyPI

---
+++ python/PYPIREADME.md
@@ -0,0 +1,6 @@
+# Momento Client Proto Generated Files for Python
+
+This package includes two Momento client proto generated files:
+
+- `cacheclient_pb2_grpc.py` - Plain cache data accessor service
+- `controlclient_pb2_grpc.py` - Plain control plane service
--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,12 @@ version = os.getenv("PYPI_MOMENTO_WIRE_TYPE_VERSION")
 
 assert version != None
 
+with open("PYPIREADME.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     version=version,
     license_files = ('../LICENSE.txt',),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
