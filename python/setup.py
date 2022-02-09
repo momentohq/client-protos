7ab0867a98a9dc09e641a79b6dc519e8c64bf06d
erikatharp <eem@momentohq.com>
2022-02-08 16:33:27 -0800

    chore: use PyPI API Auth token

--- python/setup.py
+++ python/setup.py
@@ -5,12 +5,7 @@ version = os.getenv("PYPI_MOMENTO_WIRE_TYPE_VERSION")
 
 assert version != None
 
-with open("../README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     version=version,
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     license_files = ('../LICENSE.txt',),
 )
