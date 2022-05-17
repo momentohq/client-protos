d32965e829e54a17dffb7e0e8ee2daf554eda187
Kenny <86278669+kvcache@users.noreply.github.com>
2022-05-16 22:50:53 -0700

    feat: generate grpc services for rust (#59)
    * replace dependency on environment protoc with tonic-build.
    * simplify build environment due to generating protos differently.

---
+++ rust/build.rs
@@ -0,0 +1,14 @@
+use std::io::Result;
+
+fn main() -> Result<()> {
+    tonic_build::configure()
+        .build_client(true)
+        .build_server(true)
+        .out_dir("src/")
+        .compile(
+            &["proto/controlclient.proto", "proto/cacheclient.proto", "proto/auth.proto"],
+            &["proto"],
+        )
+        .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
+    Ok(())
+}
