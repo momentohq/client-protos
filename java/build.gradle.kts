57eb30199722b62cf28526904736f1dd544374ec
Chris Price <chris@momentohq.com>
2022-07-07 08:21:44 -0700

    chore: create separate gradle project for producing java proto artifacts
    Currently, we have a single jar artifact in the "kotlin-messages"
    gradle project which bundles both java and kotlin proto artifacts.
    We are also publishing this to our internal codeartifact repo.
    
    This commit is the first of a series to clean this up and get these
    published to maven central so that users can build our OSS projects
    and we can get rid of the submodules on the Java SDK.
    
    This commit introduces a new java gradle project that builds a jar
    that contains only the java proto artifacts.  Subsequent commits
    will include:
    
    * Publishing this java protos jar artifact to maven central.
    * Removing the java protos from the kotlin jar.
    * Removing the codeartifact stuff from the kotlin jar and publishing
      that jar to maven central.
    * Updating the java SDK to get rid of the submodules and consume
      the jar from maven central.
    * Updating any places in the service code where we are referring
      to the kotlin artifact, to use the maven central coordinates
      instead.

---
+++ java/build.gradle.kts
@@ -0,0 +1,51 @@
+import com.google.protobuf.gradle.generateProtoTasks
+import com.google.protobuf.gradle.id
+import com.google.protobuf.gradle.plugins
+import com.google.protobuf.gradle.protobuf
+import com.google.protobuf.gradle.protoc
+
+plugins {
+    id("momento.java-library-conventions")
+
+    kotlin("jvm")
+    id("com.google.protobuf") version "0.8.16"
+    idea
+}
+
+val grpcProtobufVersion = "3.17.3"
+val grpcVersion = "1.39.0"
+
+dependencies {
+    implementation("io.grpc:grpc-protobuf:$grpcVersion")
+    implementation("io.grpc:grpc-stub:${grpcVersion}")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+
+    protobuf(files("../proto/"))
+}
+
+protobuf {
+    var systemOverride = ""
+    if (System.getProperty("os.name") == "Mac OS X") {
+        println("overiding protobuf artifacts classifier to osx-x86_64 so M1 Macs can find lib")
+        systemOverride = ":osx-x86_64"
+    }
+    protoc {
+        artifact = "com.google.protobuf:protoc:$grpcProtobufVersion$systemOverride"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion$systemOverride"
+        }
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                id("grpc")
+            }
+            task.generateDescriptorSet = true
+            task.descriptorSetOptions.path = "$projectDir/generated-sources/descriptors/client_protos.dsc"
+            task.descriptorSetOptions.includeImports = true
+            task.descriptorSetOptions.includeSourceInfo = true
+        }
+    }
+}
