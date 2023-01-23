e5507a6ef2c97f769e1aecf59a07f9b94f6b75a4
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2023-01-23 14:45:05 -0500

    Use tonic::include_proto instead of generating a mod.rs file

--- rust/build.rs
+++ rust/build.rs
@@ -1,9 +1,6 @@
 use std::io::Result;
-use std::path::PathBuf;
 
 fn main() -> Result<()> {
-    let out_dir = std::env::var_os("OUT_DIR").expect("OUT_DIR env var was not set");
-
     tonic_build::configure()
         .build_client(true)
         .build_server(true)
@@ -16,11 +13,6 @@ fn main() -> Result<()> {
             &["proto"],
         )
         .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
-
-    // #[path = ".."] attributes don't allow us to specify paths relative to
-    // the OUT_DIR environment variable. To work around this we generate a mod.rs
-    // file in OUT_DIR and use include! within lib.rs.
-    std::fs::copy("src/lib.tmpl.rs", PathBuf::from(out_dir).join("mod.rs"))?;
 
     Ok(())
 }
