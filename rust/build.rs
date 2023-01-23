759d885d97c2b06541aee7fd12cae625096c35fd
Sean Lynch <42618346+swlynch99@users.noreply.github.com>
2023-01-23 15:09:09 -0500

    fix: generate rust files under `$OUT_DIR` instead of `src` (#108)
    * fix: generate rust files under $OUT_DIR instead of src
    * Use tonic::include_proto instead of using an explicit mod.rs file
    
    Previously, the build script generated files under /src. This breaks
    when the cargo registry is mounted read-only, and, in any case, is not
    something that build scripts are supposed to do. The correct approach is
    to generate files under $OUT_DIR, which is guaranteed to always be
    writable and unique per crate/configuration.
    
    This commit changes the build script to build generate files under
    $OUT_DIR and then uses the standard include_proto! macro to put
    them under a predictable module name.

--- rust/build.rs
+++ rust/build.rs
@@ -4,11 +4,15 @@ fn main() -> Result<()> {
     tonic_build::configure()
         .build_client(true)
         .build_server(true)
-        .out_dir("src/")
         .compile(
-            &["proto/controlclient.proto", "proto/cacheclient.proto", "proto/auth.proto"],
+            &[
+                "proto/controlclient.proto",
+                "proto/cacheclient.proto",
+                "proto/auth.proto",
+            ],
             &["proto"],
         )
         .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
+
     Ok(())
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,11 @@
-pub mod cache_client;
-pub mod control_client;
-pub mod auth;
+pub mod auth {
+  tonic::include_proto!("auth");
+}
+
+pub mod cache_client {
+  tonic::include_proto!("cache_client");
+}
+
+pub mod control_client {
+  tonic::include_proto!("control_client");
+}
