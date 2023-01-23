a16634429a64766c536d60ec5c82a39fba5c006a
Kenny <kenny@momentohq.com>
2023-01-23 12:45:51 -0800

    chore: update rust dependencies
    * Update to the current release of tonic and friends
    * Update install_protoc script to not leave garbage in the repo directory on github
    * Add install_protoc to rust build steps - new tonic doesn't bundle it anymore

--- install_protoc.sh
+++ install_protoc.sh
@@ -8,7 +8,13 @@ sudo apt-get -y install zip tree
 curl -L https://github.com/protocolbuffers/protobuf/releases/download/v$VERSION/protoc-$VERSION-linux-x86_64.zip -o protoc.zip
 unzip -o protoc.zip -d protoc
 
-sudo mv protoc/include/* /usr/local/include/
+mkdir temp
+pushd temp
+  curl -L https://github.com/protocolbuffers/protobuf/releases/download/v$VERSION/protoc-$VERSION-linux-x86_64.zip -o protoc.zip
+  unzip -o protoc.zip -d protoc
+  sudo mv protoc/bin/* /usr/local/bin/
+  sudo mv protoc/include/* /usr/local/include/
+popd
+rm -rf temp
 
 tree /usr/local/include/google
-
