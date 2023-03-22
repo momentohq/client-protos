754915140322296185ceeb60a6a876d71212ed50
Matt Straathof <11823378+bruuuuuuuce@users.noreply.github.com>
2023-03-22 12:02:07 -0700

    fix: export new protos for javascript-web

--- javascript-web/index.ts
+++ javascript-web/index.ts
@@ -1,2 +1,4 @@
 export * as cache from './dist/CacheclientServiceClientPb'
 export * as control from './dist/ControlclientServiceClientPb'
+expost * as auth from './dist/AuthServiceClientPb'
+expost * as ping from './dist/CachepingServiceClientPb'
