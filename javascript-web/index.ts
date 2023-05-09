682a96219dc5f1ad40219a968788218c685b8e83
Matt Straathof <matt.straat@gmail.com>
2023-05-09 12:25:35 -0700

    fix: remove ts files from bundled code for js-web

--- javascript-web/index.ts
+++ javascript-web/index.ts
@@ -1,4 +1,4 @@
-export * as cache from './dist/CacheclientServiceClientPb'
-export * as control from './dist/ControlclientServiceClientPb'
-export * as auth from './dist/AuthServiceClientPb'
-export * as ping from './dist/CachepingServiceClientPb'
+export * as cache from './src/CacheclientServiceClientPb'
+export * as control from './src/ControlclientServiceClientPb'
+export * as auth from './src/AuthServiceClientPb'
+export * as ping from './src/CachepingServiceClientPb'
