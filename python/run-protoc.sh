e5223b801f61558aca5e047edeb71d71e8147f85
Michael Landis <michael@momentohq.com>
2023-05-22 17:49:00 -0700

    feat: use poetry for packaging and generate type interfaces
    Refactors the client-protos python to use poetry. This simplifies
    building, publishing, and maintaining.
    
    Also adds type hints to the generated code. Before the protoc
    invocation neither generated nor packaged type hints for the client
    protos. This made developing against them quite painful. With this we
    both generate, package, and declare the typed library typed according
    to (PEP-484)[ https://peps.python.org/pep-0484/#stub-files].

--- python/run-protoc.sh
+++ python/run-protoc.sh
@@ -2,14 +2,16 @@
 set -e
 set -x
 
+src_path=src/momento_wire_types
+
 # Generate python proto artifacts
-python -m grpc_tools.protoc -I../proto --python_out=src/momento_wire_types --grpc_python_out=src/momento_wire_types cacheclient.proto controlclient.proto auth.proto
+poetry run python -m grpc_tools.protoc -I../proto --python_out=$src_path --pyi_out=$src_path --grpc_python_out=$src_path cacheclient.proto controlclient.proto auth.proto
 
 # A shortcoming of the generated code is in the grpc generated code,
 # the protobuf imports are absolute instead of relative.
 # Left as is, the imports will fail when we install the package. See:
 # https://github.com/protocolbuffers/protobuf/issues/1491
-for grpc_src in src/momento_wire_types/*_grpc.py
+for grpc_src in $src_path/*_grpc.py
 do
     filename=$(basename "$grpc_src")
 
@@ -22,5 +24,4 @@ do
     sed -i.old "s/^\(import $grpc_module_name as \)/from . \1/g" $grpc_src
 done
 
-# Generate distribution files for PyPI
-python setup.py sdist bdist_wheel
+rm $src_path/*.old
