eca4f4ec74572b078cca846eeb3e7cc16d14a5ad
Kenny <86278669+kvcache@users.noreply.github.com>
2023-06-16 10:21:40 -0700

    chore: update rust, remove protoc user dependency (#174)
    * Move proto generation into a separate, unpublished, non-dependent sub-package.
    * Move momento-protos into explicit sub-package.
    * Create workspace.
    * Update PR checks and release publish invocations.
    * Update Tonic dependencies.
    * Remove tonic-build build-time dependency from users of momento-protos.
    * Remove protoc build-time dependency from users of momento-protos.
    
    I checked this locally with another project with a dependency on `momento-protos`.
    The import paths are unchanged, and the version bump has the intended effect.

---
+++ rust/proto_generator/build.rs
@@ -0,0 +1,28 @@
+use std::path::PathBuf;
+#[allow(clippy::unwrap_used)]
+fn main() {
+    let out_dir = PathBuf::from("../momento-protos/src");
+    let proto_dir = "../proto";
+
+    eprintln!("Hi brave developer! If you are changing protos and momento-protos fails to build, please retry 1 time.");
+    eprintln!("Cargo currently does not have a nice way for us to express a dependency order between these 2");
+    eprintln!("workspace projects - because this project is _specifically_ supposed to not be a Cargo dependency.");
+    eprintln!("We did this so downstream users don't need to have protoc when compiling momento-protos!");
+
+    tonic_build::configure()
+        .build_client(true)
+        .build_server(true)
+        .out_dir(out_dir)
+        .compile(
+            &[
+                format!("{proto_dir}/auth.proto"),
+                format!("{proto_dir}/cacheclient.proto"),
+                format!("{proto_dir}/cachepubsub.proto"),
+                format!("{proto_dir}/controlclient.proto"),
+            ],
+            &[proto_dir],
+        )
+        .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
+
+    println!("cargo:rerun-if-changed=../proto");
+}
--- rust/update_cargo_toml.py
+++ rust/update_cargo_toml.py
@@ -6,7 +6,7 @@ if len(sys.argv) != 2:
     print(sys.argv)
     sys.exit("must pass 1 positional argument, ex: python3 ./update_cargo_toml.py 1.0.1")
 
-filename = "Cargo.toml"
+filename = "momento-protos/Cargo.toml"
 
 toml_data = toml.load(filename)
 toml_data["package"]["version"] = sys.argv[1]
