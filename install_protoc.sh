a57c2b378cdbeaf0642587a8841ff7ffb19ea442
Ramya Krishnamoorthy <rmk@momentohq.com>
2023-06-28 09:22:02 -0700

    chore: Revert all changes except proto files

--- install_protoc.sh
+++ install_protoc.sh
@@ -4,22 +4,17 @@ set -x
 
 VERSION=3.18.1
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  # Install protoc for local development on MacOS (requires Homebrew)
-  brew install zip tree
-  PROTOC_ZIP=protoc-$VERSION-osx-x86_64.zip
-else # Linux
-  sudo apt-get -y install zip tree
-  PROTOC_ZIP=protoc-$VERSION-linux-x86_64.zip
-fi
+sudo apt-get -y install zip tree
+curl -L https://github.com/protocolbuffers/protobuf/releases/download/v$VERSION/protoc-$VERSION-linux-x86_64.zip -o protoc.zip
+unzip -o protoc.zip -d protoc
 
-curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v$VERSION/$PROTOC_ZIP
-sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
-sudo chmod a+rx /usr/local/bin/protoc
-sudo unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
-sudo chmod -R a+rx /usr/local/include/google
-rm $PROTOC_ZIP
+mkdir temp
+pushd temp
+  curl -L https://github.com/protocolbuffers/protobuf/releases/download/v$VERSION/protoc-$VERSION-linux-x86_64.zip -o protoc.zip
+  unzip -o protoc.zip -d protoc
+  sudo mv protoc/bin/* /usr/local/bin/
+  sudo mv protoc/include/* /usr/local/include/
+popd
+rm -rf temp
 
 tree /usr/local/include/google
-echo "Installed protoc $VERSION"
-protoc --version
