7e9333104c2d340771be51559f0f4e62df381a8e
Vir Ratanasangpunth <vir@momentohq.com>
2023-08-08 13:49:32 -0700

    feat: add vectorindex proto

--- javascript/generate_protos.sh
+++ javascript/generate_protos.sh
@@ -2,4 +2,4 @@
 set -e
 set -x
 
-PATH=node_modules/protoc-gen-ts/bin/:$PATH protoc -I=../proto -I=/usr/local/include --ts_out=src cacheclient.proto controlclient.proto cachepubsub.proto auth.proto cacheping.proto
+PATH=node_modules/protoc-gen-ts/bin/:$PATH protoc -I=../proto -I=/usr/local/include --ts_out=src cacheclient.proto controlclient.proto cachepubsub.proto auth.proto cacheping.proto vectorindex.proto
