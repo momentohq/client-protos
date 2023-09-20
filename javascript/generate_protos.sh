ad6edaea6687733b6cb5a04986612c5ddc0d12cd
Chris Price <chris@momentohq.com>
2023-09-20 16:39:22 -0700

    fix: update js generate_protos.sh to use more recent npm packages for protoc

--- javascript/generate_protos.sh
+++ javascript/generate_protos.sh
@@ -2,4 +2,9 @@
 set -e
 set -x
 
-PATH=node_modules/protoc-gen-ts/bin/:$PATH protoc -I=../proto -I=/usr/local/include --ts_out=src permissionmessages.proto cacheclient.proto controlclient.proto cachepubsub.proto auth.proto cacheping.proto vectorindex.proto token.proto
+# At some point, google removed the javascript stuff from the main protos project and separated it out into
+# its own project. This means you can't build the JS stuff with a modern version of protoc, so it can be
+# tricky to build on mac. This issue showed what I believe is the "official" way to do it now... question mark?
+#   https://github.com/protocolbuffers/protobuf/pull/9874#issuecomment-1481023982
+#
+npx grpc_tools_node_protoc -I=../proto -I=/usr/local/include --ts_out=src permissionmessages.proto cacheclient.proto controlclient.proto cachepubsub.proto auth.proto cacheping.proto vectorindex.proto token.proto
