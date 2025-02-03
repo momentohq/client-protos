c4e677d15563c7e060cce8684c28603d6f3ea511
nidhidhamnani <nidhidhamnani@gmail.com>
2025-02-03 13:06:31 -0800

    chore: rename struct

--- javascript-web/generate_protos.sh
+++ javascript-web/generate_protos.sh
@@ -70,13 +70,15 @@ function generate_proto() {
   cp -r ../proto ../proto.bak
 
   # resolves the naming conflicts between permissionmessages and permissionrules
-  sed -i 's/SuperUserPermissions/SuperUserPermissionSet/g' ../proto/permissionrules.proto
-  sed -i 's/ExplicitPermissions/ExplicitPermissionSet/g' ../proto/permissionrules.proto
+  $sed_command -i 's/SuperUserPermissions/SuperUserPermissionSet/g' ../proto/permissionrules.proto
+  $sed_command -i 's/ExplicitPermissions/ExplicitPermissionSet/g' ../proto/permissionrules.proto
+  $sed_command -i 's/SuperUser/SuperUserSet/g' ../proto/permissionrules.proto
 
   echo "Commenting out package declarations"
   for f in $proto_file_list
   do
     $sed_command 's/^\s*package \(.*\)/\/\/package \1/g' ../proto/${f}
+    $sed_command 's/permission_rules.PermissionSet/PermissionSet/g' ../proto/"${f}"
     $sed_command 's/permission_messages.Permissions/Permissions/g' ../proto/${f}
     $sed_command 's/common._Unbounded/_Unbounded/g' ../proto/${f}
     $sed_command 's/common._Empty/_Empty/g' ../proto/${f}
