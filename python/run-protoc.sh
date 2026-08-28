f75ebb5f8ab160d67f2e1d8ac8614ba0c7bb4f02
Tyler Burdsall <tyler@momentohq.com>
2026-08-28 13:31:36 -0700

    feat!: require protobuf 7 and drop the protobuf 3 code path
    BREAKING CHANGE: momento-wire-types now requires protobuf >=7.35.1,<8. Support for protobuf 3 and 4 is removed along with the v319 generated tree.

--- python/run-protoc.sh
+++ python/run-protoc.sh
@@ -3,89 +3,56 @@ set -e
 set -x
 
 package_path=momento_wire_types
+src_path=$package_path/generated
 
-# Array of versions
-# Codegen protobuf versions are bounded to match the package's own protobuf
-# constraint (>=3,<5). Leaving the upper bound off lets protoc drift to a newer
-# major and emit gencode the declared runtime cannot import.
-python_protobuf_versions=("protobuf<3.20" "protobuf>=4,<5")
-generated_code_dirs=("$package_path/v319" "$package_path/v4")
+# Generate python code from proto files against the protobuf version this package
+# declares. Older protobuf runtimes are no longer supported, so there is a single
+# generated tree rather than one per protobuf generation.
+poetry run python -m grpc_tools.protoc -I../proto --python_out=$src_path --pyi_out=$src_path --grpc_python_out=$src_path permissionmessages.proto extensions.proto cacheclient.proto controlclient.proto auth.proto cachepubsub.proto token.proto common.proto
 
-# iterate over indexes of array python_protobuf_versions
-for index in ${!python_protobuf_versions[*]}
+# A shortcoming of the generated code is in the grpc generated code,
+# the protobuf imports are absolute instead of relative.
+# Left as is, the imports will fail when we install the package. See:
+# https://github.com/protocolbuffers/protobuf/issues/1491
+for grpc_src in $src_path/*_grpc.py
 do
-    python_protobuf_version=${python_protobuf_versions[$index]}
-    src_path=${generated_code_dirs[$index]}
+    filename=$(basename "$grpc_src")
 
-    poetry add $python_protobuf_version
+    # cacheclient_pb2_grpc.py -> cacheclient_pb2
+    grpc_module_name=${filename%_grpc.py}
 
-    # grpcio-tools 1.48.2 (pinned by protobuf<3.20) imports pkg_resources, which
-    # setuptools removed in 82. Install a compatible setuptools into the venv for
-    # this step only; the lockfile keeps the current setuptools.
-    poetry run pip install --quiet 'setuptools<82'
-
-    # Generate python code from proto files
-    # The versions less than 3.20 do not support the --pyi_out flag
-    # Regardless we generate once and put in the main package location.
-    pyi_out=""
-    if [[ $python_protobuf_version == "protobuf>=4,<5" ]]; then
-        pyi_out="--pyi_out=$src_path"
-    fi
-    poetry run python -m grpc_tools.protoc -I../proto --python_out=$src_path $pyi_out --grpc_python_out=$src_path permissionmessages.proto extensions.proto cacheclient.proto controlclient.proto auth.proto cachepubsub.proto token.proto common.proto
-
-    # A shortcoming of the generated code is in the grpc generated code,
-    # the protobuf imports are absolute instead of relative.
-    # Left as is, the imports will fail when we install the package. See:
-    # https://github.com/protocolbuffers/protobuf/issues/1491
-    for grpc_src in $src_path/*_grpc.py
-    do
-        filename=$(basename "$grpc_src")
-
-        # cacheclient_pb2_grpc.py -> cacheclient_pb2
-        grpc_module_name=${filename%_grpc.py}
-
-        # Replace gRPC absolute imports with relative imports so the package
-        # will work when installed, eg:
-        # `import cacheclient_pb2 as cacheclient__pb2` -> `from . import cacheclient_pb2 as cacheclient__pb2`
-        sed -i.old "s/^\(import $grpc_module_name as \)/from . \1/g" $grpc_src
-    done
-
-    # The same shortcoming affects imports in *_pb2.py files, e.g., `cachepubsub_pb2` importing `extensions_pb2`
-    # `import extensions_pb2 as extensions__pb2` -> `from . import extensions_pb2 as extensions__pb2`
-    for pb2_src in $src_path/*_pb2.py
-    do
-        filename=$(basename "$pb2_src")
-
-        # cacheclient_pb2.py -> cacheclient_pb2
-        pb2_module_name=${filename%.py}
-
-        # Replace gRPC absolute imports with relative imports so the package
-        # will work when installed, eg:
-        # `import cacheclient_pb2` -> `from . import cacheclient_pb2`
-        sed -i.old "s/^\(import $pb2_module_name as \)/from . \1/g" $src_path/*_pb2.py
-        sed -i.old "s/^\(import $pb2_module_name as \)/from . \1/g" $src_path/*_pb2_grpc.py
-    done
-
-    rm $src_path/*.old
+    # Replace gRPC absolute imports with relative imports so the package
+    # will work when installed, eg:
+    # `import cacheclient_pb2 as cacheclient__pb2` -> `from . import cacheclient_pb2 as cacheclient__pb2`
+    sed -i.old "s/^\(import $grpc_module_name as \)/from . \1/g" $grpc_src
 done
 
-# The older versions of protobuf do not support the --pyi_out flag.
-# We can reuse the type interfaces from the newer versions without issue.
-cp $package_path/v4/*pyi $package_path/v319
+# The same shortcoming affects imports in *_pb2.py files, e.g., `cachepubsub_pb2` importing `extensions_pb2`
+# `import extensions_pb2 as extensions__pb2` -> `from . import extensions_pb2 as extensions__pb2`
+for pb2_src in $src_path/*_pb2.py
+do
+    filename=$(basename "$pb2_src")
 
-# Restore the version before running the script
-poetry add "protobuf>=3,<5"
+    # cacheclient_pb2.py -> cacheclient_pb2
+    pb2_module_name=${filename%.py}
+
+    # Replace gRPC absolute imports with relative imports so the package
+    # will work when installed, eg:
+    # `import cacheclient_pb2` -> `from . import cacheclient_pb2`
+    sed -i.old "s/^\(import $pb2_module_name as \)/from . \1/g" $src_path/*_pb2.py
+    sed -i.old "s/^\(import $pb2_module_name as \)/from . \1/g" $src_path/*_pb2_grpc.py
+done
+
+rm $src_path/*.old
 
 # Write the init template
-v4_import_string=""
-v319_import_string=""
+import_string=""
 all_string=""
-for python_src in $package_path/v4/*pb2*.py
+for python_src in $src_path/*pb2*.py
 do
     filename=$(basename "$python_src")
     module_name=${filename%.py}
-    v4_import_string="${v4_import_string}    from .v4 import $module_name as $module_name\n"
-    v319_import_string="${v319_import_string}    from .v319 import $module_name as $module_name\n"
+    import_string="${import_string}from .generated import $module_name as $module_name\n"
     # if all string is empty
     if [ -z "$all_string" ]; then
         all_string="\"$module_name\""
@@ -95,28 +62,10 @@ do
 done
 
 init_contents=$(cat <<EOF
-import google.protobuf as protobuf
-
-try:
-    protobuf_version = protobuf.__version__.split(".")
-    major = int(protobuf_version[0])
-    minor = int(protobuf_version[1])
-except ValueError:
-    raise ValueError("Could not parse protobuf version: {}".format(protobuf.__version__))
-
-# Import the correct version of the generated code.
-# To accomodate the dynamic imports, we must now write code like:
+# Re-exported so callers can write:
 #       from momento_wire_types import cacheclient_pb2 as cache_pb
 #       request = cache_pb.GetRequest()
-# instead of:
-#       from momento_wire_types.cacheclient_pb2 import GetRequest
-#       request = GetRequest()
-# This because the multi-level from will not work with the dynamic imports.
-if major >= 4 or (major == 3 and minor >= 20):
-${v4_import_string}
-else:
-${v319_import_string}
-
+${import_string}
 __all__ = [$all_string]
 EOF
 )
